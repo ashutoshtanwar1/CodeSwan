7cfb83dcf18d33ead03ef77d1c0ee2c38927c71a
Ashutosh Tanwar <ashutoshtanwar@sharechat.co>
2022-01-25 15:27:50 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
 /* eslint-disable react/style-prop-object */
-import React,{Component} from 'react';
-import './App.css';
-import Login from './Login_Page/Login';
-import Search from './Search_Contest_Page/Search';
-import Problems from './Contest_Problems_Page/Problems';
-import Pstatement from './Problem_Statement_Page/Pstatement';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React, { Component } from "react";
+import "./App.css";
+import Login from "./Login_Page/Login";
+import Search from "./Search_Contest_Page/Search";
+import Problems from "./Contest_Problems_Page/Problems";
+import Pstatement from "./Problem_Statement_Page/Pstatement";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-export default class App extends Component{
-      render(){
-        return (
-        <div> 
+export default class App extends Component {
+  render() {
+    return (
+      <div>
         <Router>
           <Switch>
-            <Route exact path = '/'>
-              <Login/>
+            <Route exact path="/">
+              <Login />
             </Route>
 
-            <Route path = '/search'>
-              <Search/>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route path="/contest">
+              <Problems />
             </Route>
 
-            <Route path = '/contest' >
-              <Problems/>
-            </Route>
-
-            <Route path = '/problem' >
-              <Pstatement/>
+            <Route path="/problem">
+              <Pstatement />
             </Route>
           </Switch>
         </Router>
-        </div> 
-          )
-      };    
+      </div>
+    );
+  }
 }
