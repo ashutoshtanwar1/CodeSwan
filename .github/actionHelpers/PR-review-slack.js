30808337ad2e9d9803a5e2f346702d2871d92b03
Ashutosh Tanwar <ashutoshtanwar@sharechat.co>
2022-01-25 16:35:15 +0530

    Changes#1

--- .github/actionHelpers/PR-review-slack.js
+++ .github/actionHelpers/PR-review-slack.js
@@ -1,7 +1,44 @@
-module.exports = async ({ github, context, core }) => {
+module.exports = async ({ github, context }) => {
   try {
-    console.log(github, context, core);
-    // console.log();
+    const { label = {}, pull_request = {}, sender = {} } = context.payload;
+    const {
+      additions,
+      changed_files,
+      deletions,
+      html_url: prUrl,
+      title,
+    } = pull_request;
+    const { html_url: userUrl, login: userName } = sender;
+    const isUrgent = (label.name || "").includes("Urgent");
+    const msgToBeSend = `<${userUrl}|${userName}> requested PR Review ${
+      isUrgent && "URGENTLY"
+    }\n\n${title} - (+${additions} , -${deletions} , ${changed_files} files changed.)\n\n<${prUrl}|VIEW PR>`;
+
+    console.log(contest.issue, contest.repo);
+
+    console.log(
+      msgToBeSend,
+      "\n\nMERGED:",
+      github.pulls.checkIfMerged({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+      })
+    );
+
+    github.issues.addAssignees({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: context.issue.number,
+      assignees: ["assignees"],
+    });
+
+    github.pulls.requestReviewers({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: context.issue.number,
+      reviewers: ["reviewers"],
+    });
   } catch (err) {
     console.log("err : ", err);
   }
